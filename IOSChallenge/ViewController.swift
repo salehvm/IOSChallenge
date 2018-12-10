b933de980c01fa8c8b63c390f649f1181b13f5d4
saleh <salehvm@code.edu.az>
2018-12-10 11:46:27 +0400

    First commit for push project

---
+++ IOSChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSChallenge
+//
+//  Created by Saleh Majıdov on 12/10/18.
+//  Copyright © 2018 Saleh Majıdov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
